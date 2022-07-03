af1ec4546f1a6bbacb58ae7e1cf46f169deb8802
alemiranda7 <7alemiranda@gmail.com>
2022-07-03 13:45:11 -0300

    update ok

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import React from 'react'
 const titulo = <h1>Titulo com variavel</h1>
 const random = Math.random()
 
+function useRandom() {
+    return Math.random()
+}
 
 const App = () => {
     
+    
+
     return (
         <>
         {titulo}
@@ -13,6 +18,8 @@ const App = () => {
         <p className='random1' title='numero multiplicado por 100'>{random * 100}</p>
 
         <p className='random2' title='numero gerado aleatorio com titulo'>{random}</p>
+
+        <p title='numero aleatorio com função'>{useRandom()}</p>
         
         <p className='corpoText'> 
             <a href="/"> aqui é o corpo do texto</a>
